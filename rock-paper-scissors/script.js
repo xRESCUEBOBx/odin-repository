2dcc761d7fb0a27c61dbc51c34f8236dfc8a7cfb
xRESCUEBOBx <dlsjr80@gmail.com>
2023-06-19 13:41:18 -0500

    Made some changes to FIZZBUZZ

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,6 +1,5 @@
 function getComputerMove(computerMove) {
   let move = Math.random();
-  console.log(move);
 
   if (move <= 1 / 3) {
     computerMove = "rock";
@@ -9,7 +8,55 @@ function getComputerMove(computerMove) {
   } else if (move > 2 / 3) {
     computerMove = "scissors";
   }
-  console.log(computerMove);
+  console.log("Computer picked " + computerMove);
+  return computerMove;
 }
 
-console.log(getComputerMove());
+function getPlayerMove(playerMove) {
+  const computerMove = getComputerMove();
+  let result = "";
+
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
+      result = "Tie!";
+    } else if (computerMove === "paper") {
+      result = "You Lose!";
+    } else if (computerMove === "scissors") {
+      result = "You Win!";
+    }
+    console.log(result);
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You Win!";
+    } else if (computerMove === "paper") {
+      result = "Tie!";
+    } else if (computerMove === "scissors") {
+      result = "You Lose!";
+    }
+    console.log(result);
+  } else if (playerMove === "scissors") {
+    if (computerMove === "rock") {
+      result = "You lose!";
+    } else if (computerMove === "paper") {
+      result = "You Win!";
+    } else if (computerMove === "scissors") {
+      result = "Tie!";
+    }
+    console.log(result);
+  }
+
+  console.log("You picked " + playerMove);
+
+  return playerMove;
+}
+
+document.querySelector(".js-rock-button").addEventListener("click", () => {
+  getPlayerMove("rock");
+});
+
+document.querySelector(".js-paper-button").addEventListener("click", () => {
+  getPlayerMove("paper");
+});
+document.querySelector(".js-scissors-button").addEventListener("click", () => {
+  getPlayerMove("scissors");
+});
