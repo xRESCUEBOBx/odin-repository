0baa269c41a39289905d555e623ca7192d812dc4
xRESCUEBOBx <dlsjr80@gmail.com>
2023-06-22 18:09:32 -0500

    Finished the rock paper scissors project

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,67 @@
+let playerScore = Number("");
+let computerScore = Number("");
+
+function playGame(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+
+  playerSelection = prompt("rock paper or scissors?");
+  let result;
+
+  if (playerSelection.toLowerCase() === "rock") {
+    if (computerSelection === "rock") {
+      result = "You Tied!";
+      console.log(`${result}, Gonna let it end like this..`);
+    } else if (computerSelection === "paper") {
+      result = "You Lose!";
+      console.log(`${result}, Too Bad Try Again!`);
+    } else if (computerSelection === "scissors") {
+      result = "You Win!";
+      console.log(`${result}, Great Job Play Another!`);
+    }
+  }
+
+  if (playerSelection.toLowerCase() === "paper") {
+    if (computerSelection === "rock") {
+      result = "You Win!";
+      console.log(`${result}, Great Job Play Another!`);
+    } else if (computerSelection === "paper") {
+      result = "You Tied!";
+      console.log(`${result}, Gonna let it end like this..`);
+    } else if (computerSelection === "scissors") {
+      result = "You Lose!";
+      console.log(`${result}, Too Bad Try Again!`);
+    }
+  }
+
+  if (playerSelection.toLowerCase() === "scissors") {
+    if (computerSelection === "rock") {
+      result = "You Lose!";
+      console.log(`${result}, Too Bad Try Again!`);
+    } else if (computerSelection === "paper") {
+      result = "You Win!";
+      console.log(`${result}, Great Job Play Another!`);
+    } else if (computerSelection === "scissors") {
+      result = "You Tied!";
+      console.log(`${result}, Gonna let it end like this..`);
+    }
+  }
+
+  if (result === "You Win!") {
+    playerScore++;
+  } else if (result === "You Lose!") {
+    computerScore++;
+  }
+
+  console.log(`You picked ${playerSelection}, 
+  computer picked ${computerSelection}.
+  The Score is currently
+  player:${playerScore} 
+  computer${computerScore}.
+  `);
+
+  return result;
+}
+
 function getComputerChoice(computerChoice) {
   computerChoice = Math.random();
 
@@ -5,11 +69,22 @@ function getComputerChoice(computerChoice) {
     computerChoice = "rock";
   } else if (computerChoice > 1 / 3 && computerChoice <= 2 / 3) {
     computerChoice = "paper";
-  } else if (computerChoice > 2 / 3) {
+  } else {
     computerChoice = "scissors";
   }
-
+  console.log(computerChoice);
   return computerChoice;
 }
 
-console.log(getComputerChoice());
+for (let i = 0; i <= 100; i++) {
+  playGame();
+
+  if (playerScore === 5 || computerScore === 5) {
+    console.log(`Game Over X./'^'\.X
+    Player Score:${playerScore}
+    Computer Score${computerScore}
+    Refresh the page to play again!
+    `);
+    break;
+  }
+}
