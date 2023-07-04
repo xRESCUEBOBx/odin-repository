34d3214163a33366a812d414435545db44e01698
xRESCUEBOBx <dlsjr80@gmail.com>
2023-07-04 16:43:07 -0500

    Almost finished.

--- Etch-a-Sketch/javascript.js
+++ Etch-a-Sketch/javascript.js
@@ -2,6 +2,32 @@ const divElements = [];
 const divElementsContainer = document.createElement("div");
 document.body.appendChild(divElementsContainer);
 divElementsContainer.classList.add("div-elements-container");
+const buttonElement = document.querySelector("button");
+
+buttonElement.addEventListener("click", () => {
+  switch (true) {
+    case divElementsContainer.classList.contains("div-elements-container"):
+      divElementsContainer.classList.remove("div-elements-container");
+      divElementsContainer.classList.add("size2");
+      break;
+    case divElementsContainer.classList.contains("size2"):
+      divElementsContainer.classList.remove("size2");
+      divElementsContainer.classList.add("size3");
+      break;
+    case divElementsContainer.classList.contains("size3"):
+      divElementsContainer.classList.remove("size3");
+      divElementsContainer.classList.add("size4");
+      break;
+    case divElementsContainer.classList.contains("size4"):
+      divElementsContainer.classList.remove("size4");
+      divElementsContainer.classList.add("size5");
+      break;
+    default:
+      divElementsContainer.classList.remove("size5");
+      divElementsContainer.classList.add("div-elements-container");
+      break;
+  }
+});
 
 for (let i = 0; i <= 255; i++) {
   const div = document.createElement("div");
@@ -22,6 +48,8 @@ for (let i = 0; i <= 255; i++) {
     if (isDrawing) {
       this.classList.add("drawn");
       // Add the "drawn" class
+    } else {
+      this.classList.remove("drawn");
     }
   });
 }
