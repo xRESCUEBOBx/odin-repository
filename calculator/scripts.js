b466789135daa51c141bccfa989a08e9d521f55d
xRESCUEBOBx <dlsjr80@gmail.com>
2023-07-16 12:21:55 -0500

    Finished project

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -1,46 +1,113 @@
+// JavaScript (scripts.js)
 let firstOperand = null;
 let operator = null;
 let secondOperand = null;
-const displayElement = document.querySelector(".display");
-const buttonElements = document.querySelectorAll("button");
+let result = null;
 
-for (let button of buttonElements) {
+const displayElement = document.querySelector(".display");
+const clearButton = document.querySelector(".clear");
+const equalButton = document.querySelector(".equals");
+
+const operatorButtons = document.querySelectorAll(".operator");
+const numberButtons = document.querySelectorAll(
+  ".calculator button:not(.operator, .clear, .equals)"
+);
+
+clearButton.addEventListener("click", () => {
+  clear();
+  displayElement.textContent = "";
+});
+
+for (const button of numberButtons) {
   button.addEventListener("click", () => {
-    if (/\d/.test(button.textContent)) {
-      displayElement.textContent += button.textContent;
+    const buttonText = button.textContent;
+    displayElement.textContent += buttonText;
+  });
+}
+
+for (const operatorButton of operatorButtons) {
+  operatorButton.addEventListener("click", () => {
+    const operatorText = operatorButton.textContent;
+    if (["+", "-", "x", "÷", "%"].includes(operatorText)) {
+      if (firstOperand !== null && operator !== null) {
+        secondOperand = parseFloat(displayElement.textContent);
+        calculate();
+        firstOperand = result;
+        operator = operatorText; // Save the operator value
+      } else {
+        firstOperand = parseFloat(displayElement.textContent);
+        operator = operatorText; // Save the operator value
+      }
+      displayElement.textContent = "";
     }
   });
 }
 
-const add = function (a, b) {
+equalButton.addEventListener("click", () => {
+  if (firstOperand !== null && operator !== null) {
+    secondOperand = parseFloat(displayElement.textContent);
+    calculate();
+    displayElement.textContent = result;
+    firstOperand = result;
+    operator = null;
+    secondOperand = null;
+  }
+});
+const plusMinusButton = document.querySelector(".plusMinus");
+
+plusMinusButton.addEventListener("click", () => {
+  if (displayElement.textContent !== "") {
+    const currentValue = parseFloat(displayElement.textContent);
+    displayElement.textContent = (-currentValue).toString();
+  }
+});
+
+function add(a, b) {
   return a + b;
-};
-
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const sum = function (a) {
-  return a.reduce((a, b) => a + b, 0);
-};
-
-const multiply = function (...a) {
-  let product = 1;
-  for (let i = 0; i < a.length; i++) {
-    product *= a[i];
-  }
-  return product;
-};
-const divide = function (...a) {
-  let product = 1;
-  for (let i = 0; i < a.length; i++) {
-    product /= a[i];
-  }
-  return product;
-};
-
-function operate(operand1, operator, operand2) {
-  return add(operand1, operand2);
 }
 
-console.log(operate());
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) throw new Error("Cannot divide by zero.");
+  return a / b;
+}
+
+function remainder(a, b) {
+  return (a * b) / 100;
+}
+
+function calculate() {
+  switch (operator) {
+    case "+":
+      result = add(firstOperand, secondOperand);
+      break;
+    case "-":
+      result = subtract(firstOperand, secondOperand);
+      break;
+    case "x":
+      result = multiply(firstOperand, secondOperand);
+      break;
+    case "÷":
+      result = divide(firstOperand, secondOperand);
+      break;
+    case "%":
+      result = remainder(firstOperand, secondOperand);
+      break;
+    default:
+      throw new Error("Unknown operator.");
+  }
+}
+
+function clear() {
+  firstOperand = null;
+  operator = null;
+  secondOperand = null;
+  result = null;
+}
