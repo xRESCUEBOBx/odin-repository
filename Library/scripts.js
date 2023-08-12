d2d206765e8cae3e95f81c2471ec7dfc2c8882ec
xRESCUEBOBx <dlsjr80@gmail.com>
2023-08-11 19:59:47 -0500

    Finished My Library

--- Library/scripts.js
+++ Library/scripts.js
@@ -10,58 +10,77 @@ const pagesInputElement = document.querySelector("#pages");
 const readInputElement = document.querySelector("#read");
 const dontSubmitButtonElement = document.querySelector("#submit");
 const submitButtonElement = document.querySelector(".submit");
-
-formContainerElement.style.display = "none";
+const deleteButton = document.createElement("button");
 
 addBookButtonElement.addEventListener("click", () => {
-  if (formContainerElement.style.display === "none") {
-    formContainerElement.style.display = "block";
-  } else {
-    formContainerElement.style.display = "none";
+  formContainerElement.style.display =
+    formContainerElement.style.display === "none" ? "block" : "none";
+});
+
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
+
+bookDisplayElement.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-book")) {
+    const index = e.target.getAttribute("data-index");
+    removeBookFromLibrary(index);
+  } else if (e.target.classList.contains("toggle-read")) {
+    const index = e.target.getAttribute("data-index");
+    toggleReadStatus(index);
   }
 });
 
-dontSubmitButtonElement.addEventListener("click", preventDefault, false);
-submitButtonElement.addEventListener("click", addBookToLibrary);
-
-function Books(title, author, pages, read) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-Books.prototype.getInfo = function () {
-  return `${this.title} ${this.author} ${this.pages} ${this.read}`;
+Book.prototype.getInfo = function () {
+  return `Title:${this.title} => Author:${this.author} => Pages:${this.pages} => Read:${this.read}`;
 };
 
 function addBookToLibrary() {
-  let input = new Books(
-    titleInputElement.value,
-    authorInputElement.value,
-    pagesInputElement.value,
-    readInputElement.value
-  );
+  const title = titleInputElement.value;
+  const author = authorInputElement.value;
+  const pages = pagesInputElement.value;
+  const read = readInputElement.value;
 
-  if (titleInputElement.value !== "" && authorInputElement.value !== "") {
-    myLibrary.push(input);
-  } else if (
-    titleInputElement.value === "" &&
-    authorInputElement.value === ""
-  ) {
+  if (title !== "" && author !== "") {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayBooks();
+  } else {
     titleInputElement.placeholder = "Must input title!";
     authorInputElement.placeholder = "Must input author!";
   }
 
-  myLibrary.forEach((book) => {
-    const bookDisplay = document.createElement("div");
-    bookDisplay.textContent = `${input.title} ${input.author} ${input.pages} ${input.read} `;
-    bookDisplayElement.appendChild(bookDisplay);
-  });
-
   console.log(myLibrary);
 }
 
-function preventDefault(event) {
-  event.preventDefault();
+function displayBooks() {
+  bookDisplayElement.textContent = "";
+
+  myLibrary.forEach((book, index) => {
+    const bookDisplay = document.createElement("div");
+    bookDisplay.classList.add("book-card");
+    bookDisplay.innerHTML = `
+      <p>${book.getInfo()}</p>
+      <button class="remove-book" data-index="${index}">Remove</button>
+      <button class="toggle-read" data-index="${index}">Toggle Read</button>
+    `;
+    bookDisplayElement.appendChild(bookDisplay);
+  });
+}
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+function toggleReadStatus(index) {
+  myLibrary[index].read =
+    myLibrary[index].read === "Read" ? "Not Read" : "Read";
+  displayBooks();
 }
